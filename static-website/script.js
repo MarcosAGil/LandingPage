95e9f4dc4270f782d88db5025baf2dbcc6b7316d
Marcos A.M <127544044+MarcosAGil@users.noreply.github.com>
2025-06-16 07:41:03 +0200

    Update script.js

--- static-website/script.js
+++ static-website/script.js
@@ -4,7 +4,7 @@
 const GOOGLE_SHEET_ID = '11kMQ3nh3P51qgp1qGMurOSzFlJh4g7X0mUCHkdgxa0g';
 
 // Google Apps Script deployment URL - REPLACE WITH YOUR ACTUAL URL
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz5n3Y1_TnRppwLIBW_5qVpwNGiKn7otoKw7zBzX7kMAsAwcm-106A0wl2GLnXmWSEV/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw1GdKgNfhnQ3mpknxkcXwKoznG-vGVueqfKp4f4XUhdnEHh7qVAhvhkdOnwzW-VeLvKA/exec';
 
 // Form submission handler
 document.addEventListener('DOMContentLoaded', function() {
